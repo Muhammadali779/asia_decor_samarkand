d1f82e21a4d059b13ecef58af80807053b0328e8
Muhammadali <baxtiyorovich779@gmail.com>
2026-04-11 00:28:33 +0500

    complete

--- asia_decor/settings.py
+++ asia_decor/settings.py
@@ -112,11 +112,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+WHITENOISE_MANIFEST_STRICT = False
 
 
 MEDIA_URL = '/media/'
